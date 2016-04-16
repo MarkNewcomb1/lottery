ccff93a98ededdd350f60fcd568eac000168a534
MarkNewcomb <marknewcomb@Marks-MacBook-Pro.local>
2016-04-16 15:51:18 -0600

    number check works, array doesn't

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 //user starts with $10
 var money = 10;
-var num1 = document.getElementById("num1").value;
-var num2 = document.getElementById("num2").value;
-var num3 = document.getElementById("num3").value;
-var num4 = document.getElementById("num4").value;
+var num1 = parseInt(document.getElementById("num1").value);
+var num2 = parseInt(document.getElementById("num2").value);
+var num3 = parseInt(document.getElementById("num3").value);
+var num4 = parseInt(document.getElementById("num4").value);
 
 var output = document.getElementById("output");
 
 var playButton = document.getElementById("play");
 function pressButton () {
     if (money < 2) {
-        output.innerHTML = "<span class='error'>Sorry, you don't have enough money to play</span>";
+        output.innerHTML = "<span class='error'>Sorry, you don't have enough money to play.</span>";
         return false;
     }
-    if (num1 !== parseInt(num1) || num2 !== parseInt(num2) || num3 !== parseInt(num3)  || num4 !== parseInt(num4) ) {
-      output.innerHTML = "<span class='error'>Please pick a number.</span>";
+    if (typeof num1 != "number" || typeof num2 != "number" || typeof num3 != "number"  || typeof num4 != "number" ) {
+      output.innerHTML = "<span class='error'>Please pick a number and make sure all 4 boxes have numbers in them.</span>";
         console.log("That's not a number");
         return false;
     }
+    var arrayOfNumbersPlayed = [num1, num2, num3, num4];
+    
     money -= 2;
     console.log("You pressed the button!");
     console.log(money);
-    console.log(typeof(num1));
+    console.log(arrayOfNumbersPlayed);
 }
 	
 
