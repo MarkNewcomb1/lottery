f04061b7569f2d3d98de11b39a39fe49c281e229
MarkNewcomb <marknewcomb@Marks-MacBook-Pro.local>
2016-04-16 14:12:00 -0600

    button decreases money, error if not enough money

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ var num2 = document.getElementById("num2").value;
 var num3 = document.getElementById("num3").value;
 var num4 = document.getElementById("num4").value;
 
+var output = document.getElementById("output");
+
 var playButton = document.getElementById("play");
 function pressButton () {
+    if (money < 2) {
+        output.innerHTML = "<span class='error'>Sorry, you don't have enough money to play</span>";
+        return false;
+    }
+    money -= 2;
     console.log("You pressed the button!");
+    console.log(money);
 }
 	
 
